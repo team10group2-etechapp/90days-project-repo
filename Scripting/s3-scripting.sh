335a9d5b3d33d5352cd0ab7ae1eb47d345677ec8
Peter Ntsambi <125835309+mbohlip@users.noreply.github.com>
2024-07-29 02:29:49 -0700

    update s3 scripting code (#3)
    * commit terraform code project from team10-group2-EtechApp
    
    * Commit s3 shell scripting
    
    * Update s3 scripting code to include count of bucket prefixes

--- Scripting/s3-scripting.sh
+++ Scripting/s3-scripting.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 read -p "Enter your github ssh repository: " github_repo
-git clone $github_repo
+git clone git@github.com:team10group2-etechapp/${github_repo}.git
 echo "Repository clone completed ..."
+cd ${github_repo}/terraform-code/
 read -p "Enter s3 bucket name: " bucket_name
 aws s3 mb s3://${bucket_name} --region us-east-1
 read -p "enter your bucket prefix: " dir
-aws s3 sync terraform-code s3://${s3-bucket-name}/$dir/
+aws s3 cp . s3://${bucket_name}/$dir/ --recursive
+
+# Count the number of prefixes in bucket created above
+aws s3 ls s3://${bucket_name}/ | wc -l
